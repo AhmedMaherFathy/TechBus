212c54ed0b141d77d4b960796fa53d89c1d00373
AhmedMaherFathy <ahmedmaher.xd8551@gmail.com>
2024-11-30 15:46:03 +0200

    try solve error

--- Modules/Admin/Routes/web.php
+++ Modules/Admin/Routes/web.php
@@ -14,6 +14,6 @@ use Modules\Admin\Http\Controllers\AdminController;
 |
 */
 
-Route::group([], function () {
-    Route::resource('admin', AdminController::class)->names('admin');
-});
+// Route::group([], function () {
+//     Route::resource('admin', AdminController::class)->names('admin');
+// });
