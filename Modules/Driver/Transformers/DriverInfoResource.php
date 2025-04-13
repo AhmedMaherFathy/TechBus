b9ab790d6837c7c7220db74f9352874c095702ac
AhmedMaherFathy <ahmedmaher.xd8551@gmail.com>
2025-04-13 22:37:55 +0200

    check problem

--- Modules/Driver/Transformers/DriverInfoResource.php
+++ Modules/Driver/Transformers/DriverInfoResource.php
@@ -15,21 +15,21 @@ class DriverInfoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $plate_number = $plate_string = null;
-        if (!isNull($this->bus->plate_number)){
-            info($this->bus->plate_number);
-            $plate = explode("-",$this->bus->plate_number);
-            $plate_number = $plate[0];
-            $plate_string = $plate[1];
-        }
+        // $plate_number = $plate_string = null;
+        // if (!isNull($this->bus->plate_number)){
+        //     info($this->bus->plate_number);
+        //     $plate = explode("-",$this->bus->plate_number);
+        //     $plate_number = $plate[0];
+        //     $plate_string = $plate[1];
+        // }
         return [
             "fullName" => $this->full_name,
             "startTime" => $this->formattedStartTime,
             "endTime" => $this->formattedEndTime,
             "workingDays" => $this->days,
             "busInfo" =>[
-                    "plateNumbers" => $plate_number,
-                    "plateString" => $plate_string,
+                    // "plateNumbers" => $plate_number,
+                    // "plateString" => $plate_string,
                     "routeNumber" => optional(optional($this->bus)->route)->number,
             ]
         ];
