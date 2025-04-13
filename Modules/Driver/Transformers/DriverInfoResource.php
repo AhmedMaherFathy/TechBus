acf813982e5e6014e0059c7582e2ead9220376dc
AhmedMaherFathy <ahmedmaher.xd8551@gmail.com>
2025-04-13 22:43:10 +0200

    solve driver info error

--- Modules/Driver/Transformers/DriverInfoResource.php
+++ Modules/Driver/Transformers/DriverInfoResource.php
@@ -15,22 +15,21 @@ class DriverInfoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        // $plate_number = $plate_string = null;
-        // if (!isNull($this->bus->plate_number)){
-        //     info($this->bus->plate_number);
-        //     $plate = explode("-",$this->bus->plate_number);
-        //     $plate_number = $plate[0];
-        //     $plate_string = $plate[1];
-        // }
+        $plate_number = $plate_string = null;
+        if (!is_null($this->bus->plate_number)){
+            $plate = explode("-",$this->bus->plate_number);
+            $plate_number = $plate[0] ?? null;
+            $plate_string = $plate[1] ?? null;
+        }
         return [
             "fullName" => $this->full_name,
             "startTime" => $this->formattedStartTime,
             "endTime" => $this->formattedEndTime,
             "workingDays" => $this->days,
             "busInfo" =>[
-                    // "plateNumbers" => $plate_number,
-                    // "plateString" => $plate_string,
-                    "routeNumber" => optional(optional($this->bus)->route)->number,
+                    "plateNumbers" => $plate_number,
+                    "plateString" => $plate_string,
+                    "routeNumber" => $this->bus?->route?->number,
             ]
         ];
     }
