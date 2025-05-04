f54a579fba86dd6182e1273f1712d49e7845e06f
AhmedMaherFathy <ahmedmaher.xd8551@gmail.com>
2025-05-04 22:49:40 +0300

    refactor response

--- Modules/Notification/Http/Controllers/NotificationController.php
+++ Modules/Notification/Http/Controllers/NotificationController.php
@@ -3,12 +3,14 @@
 namespace Modules\Notification\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Traits\HttpResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Modules\Notification\Notifications\TestNotification;
 
 class NotificationController extends Controller
 {
+    use HttpResponse;
     public function makeNotification(Request $request)
     {
         $request->user('driver')->notify(new TestNotification());
@@ -23,6 +25,6 @@ class NotificationController extends Controller
             ];
         });
         
-        return response()->json($notifications);
+        return $this->successResponse(data:$notifications);
     }
 }
